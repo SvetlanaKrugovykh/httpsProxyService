88a737e3d189470aa0619d11497fa79d8d864946
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2023-11-08 17:10:30 +0200

    FOR THE PROXY SAVING

--- src/server.js
+++ src/server.js
@@ -1,27 +1,81 @@
 const http = require('http')
 const https = require('https')
+const httpProxy = require('http-proxy')
+
 require('dotenv').config()
-const { data, certProxy } = require('./data/netData.js')
-const handleProxyRequest = require('./services/proxyHandler.js')
+const { data, certProxy, certTarget } = require('./data/netData.js')
+const { handleRequest, handleProxyHttpsRequest } = require('./services/proxyHandler.js')
 const currentTime = new Date().toLocaleString('en-US', { dateStyle: 'medium', timeStyle: 'short' })
 
 const credentials = { key: certProxy.key, cert: certProxy.cert }
+const targetCredentials = { key: certTarget.key, cert: certTarget.cert }
 
 for (const netData of data) {
 
-  const server = https.createServer(credentials, (req, res) => {
-    handleProxyRequest(req, res, {
+  const options = {
+    target: netData.target,
+    ssl: credentials,
+    secure: false,
+    changeOrigin: true,
+  }
+
+  const server_http = http.createServer((req, res) => {
+    handleRequest(req, res, {
+      target: netData.target,
+      port: netData.target_port,
+      secure: false,
+    }, netData)
+  })
+
+  const proxy = httpProxy.createProxyServer(options)
+
+  const proxyServer = https.createServer({
+    ...credentials,
+    ciphers: [
+      'ECDHE-RSA-AES128-GCM-SHA256',
+      'ECDHE-ECDSA-AES128-GCM-SHA256',
+      'ECDHE-RSA-AES256-GCM-SHA384',
+      'ECDHE-ECDSA-AES256-GCM-SHA384',
+      'DHE-RSA-AES128-GCM-SHA256',
+      'ECDHE-RSA-AES128-SHA256',
+      'DHE-RSA-AES128-SHA256',
+      'ECDHE-RSA-AES256-SHA384',
+      'DHE-RSA-AES256-SHA384',
+      'ECDHE-RSA-AES256-SHA256',
+      'DHE-RSA-AES256-SHA256',
+      'HIGH',
+      '!aNULL',
+      '!eNULL',
+      '!EXPORT',
+      '!DES',
+      '!RC4',
+      '!MD5',
+      '!PSK',
+      '!SRP',
+      '!CAMELLIA'
+    ].join(':'),
+    secureProtocol: 'TLSv1_2_method',
+  }, (req, res) => {
+    proxy.web(req, res)
+  })
+
+  proxy.on('proxyReq', (proxyReq, req, res, options) => {
+    handleProxyHttpsRequest(req, res, {
       target: netData.target,
       port: netData.target_port,
       secure: true,
     }, netData)
   })
 
-  const server_http = http.createServer((req, res) => {
-    handleProxyRequest(req, res, {
+  proxy.on('error', function (err, req, res) {
+    console.error('Proxy error:', err);
+  })
+
+  const server = https.createServer(credentials, (req, res) => {
+    handleRequest(req, res, {
       target: netData.target,
       port: netData.target_port,
-      secure: false,
+      secure: true,
     }, netData)
   })
 
@@ -30,7 +84,11 @@ for (const netData of data) {
   })
 
   server_http.listen(netData.port_http, netData.server_node, () => {
-    console.log(`Proxy http server listening at ${currentTime} on https://${netData.server_node}:${netData.port_http}`)
+    console.log(`Proxy http server listening at ${currentTime} on http://${netData.server_node}:${netData.port_http}`)
+  })
+
+  proxyServer.listen(netData.port_https_proxy, netData.server_node, () => {
+    console.log(`Https Proxy server listening at ${currentTime} on https://${netData.server_node}:${netData.port_https_proxy}`)
   })
 
 }
--- src/services/proxyHandler.js
+++ src/services/proxyHandler.js
@@ -1,10 +1,10 @@
 
-async function handleProxyRequest(req, res, netData) {
+async function handleRequest(req, res, netData) {
   try {
     const currentTime = new Date().toLocaleString('en-US', { dateStyle: 'medium', timeStyle: 'short' })
     const Location = netData.target + 'redirect.html'
 
-    console.log(`Proxy request start at ${currentTime}:`, req.rawHeaders[1])
+    console.log(`Request start at ${currentTime}:`, req.rawHeaders[1])
 
     res.writeHead(302, {
       'Location': Location,
@@ -15,4 +15,21 @@ async function handleProxyRequest(req, res, netData) {
   }
 }
 
-module.exports = handleProxyRequest 
+async function handleProxyHttpsRequest(req, res, netData) {
+  try {
+    const currentTime = new Date().toLocaleString('en-US', { dateStyle: 'medium', timeStyle: 'short' })
+    console.log(`Proxy request start at ${currentTime}:`, req.rawHeaders[1])
+
+    const redirectUrl = netData.target + 'redirect.html'
+
+    res.writeHead(302, {
+      'Location': redirectUrl,
+    })
+    res.end()
+  } catch (error) {
+    console.error('Error while handling request:', error)
+  }
+}
+
+
+module.exports = { handleRequest, handleProxyHttpsRequest }
